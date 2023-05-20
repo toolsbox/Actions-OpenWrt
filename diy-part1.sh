e902d78b227e2124366f931663d4fcee18475442
toolsbox <73782898+toolsbox@users.noreply.github.com>
2023-05-20 23:27:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,10 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+#passwall
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
